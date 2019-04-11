38037ae3d14f6373c8c431cfee40d3a3786e20f6
Kaczmarczyk.Michal <209344@edu.p.lodz.pl>
2019-04-11 16:42:28 +0200

    isolation added stateTestRequestInvoiceWithOnePosition test

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,57 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class BookKeeperTest {
+
+    private InvoiceRequest invoiceRequest;
+    private InvoiceFactory invoiceFactory;
+    private BookKeeper bookKeeper;
+    private RequestItem requestItem;
+    private ClientData clientData;
+    private ProductData productData;
+    private TaxPolicy taxPolicy;
+
+    @Before
+    public void initialize() {
+        clientData = new ClientData(Id.generate(), "client");
+        invoiceRequest = new InvoiceRequest(clientData);
+        invoiceFactory = new InvoiceFactory();
+        bookKeeper = new BookKeeper(invoiceFactory);
+    }
+
+    @Test
+    public void stateTestRequestInvoiceWithOnePosition() {
+        taxPolicy = mock(TaxPolicy.class);
+        productData = mock(ProductData.class);
+        requestItem = new RequestItem(productData, 1, new Money(100));
+
+        invoiceRequest.add(requestItem);
+
+        when(productData.getType()).thenReturn(ProductType.STANDARD);
+        when(taxPolicy.calculateTax(ProductType.STANDARD, new Money(100))).thenReturn(new Tax(new Money(100), "tax"));
+
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        int result = invoice.getItems().size();
+
+        Assert.assertThat(result, is(1));
+    }
+
+}
